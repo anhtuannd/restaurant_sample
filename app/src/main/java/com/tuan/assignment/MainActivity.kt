932964ed5f28f8bd95abf70ebef00592da78f854
anhtuannd <zenlak@gmail.com>
2024-07-16 15:05:59 +0200

    Fix edge to edge display

--- app/src/main/java/com/tuan/assignment/MainActivity.kt
+++ app/src/main/java/com/tuan/assignment/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.tuan.assignment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
@@ -18,12 +20,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
 
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
 
         setContent {
-            WindowInsets.safeDrawing
             UmainAssignmentTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
